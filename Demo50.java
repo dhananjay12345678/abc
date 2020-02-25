6fd7c470baab78762a616887e0ece5cf04545c7e
dhananjay12345678 <61458854+dhananjay12345678@users.noreply.github.com>
2020-02-25 17:04:21 +0530

    Add files via upload

---
+++ Demo50.java
@@ -0,0 +1,21 @@
+abstract class A
+{
+class B
+{
+void show()
+{
+System.out.println("rz");
+}
+}
+class C
+{}
+}
+class Demo50
+{
+public static void main(String ar[])
+{
+C c=new C();
+C.B n=c.new B();
+b.show();
+}
+}
